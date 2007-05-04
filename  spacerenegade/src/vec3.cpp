bb607a76a77526ed2c5edc09c9a3e37436b5d0bd
jamoozy <jamoozy@d98d4265-742f-0410-abd1-c3b8cf015911>
2007-05-04 09:09:27 +0000

    Corrected dot and cross products, added scalar product, added normalize function, and made all functions that do not alter "this" say so, by putting a "const" in the right place.

---  spacerenegade/src/vec3.cpp
+++  spacerenegade/src/vec3.cpp
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <cmath>
 #include "vec3.h"
 
 
@@ -6,7 +7,7 @@
 Vec3::Vec3() : _x(0), _y(0), _z(0) {}
 
 // This constructor initializes the vector to the given point.
-Vec3::Vec3(double a, double b, double c) : _x(a), _y(b), _z(c) {}
+Vec3::Vec3(const double a, const double b, const double c) : _x(a), _y(b), _z(c) {}
 
 // This constructor initializes the vector to another vector.
 Vec3::Vec3(const Vec3& a) : _x(a._x), _y(a._y), _z(a._z) {}
@@ -16,17 +17,29 @@ double Vec3::x() { return _x; }
 double Vec3::y() { return _y; }
 double Vec3::z() { return _z; }
 
+// Make this vector's length 1.
+void Vec3::normalize()
+{
+	double norm = sqrt(_x * _x + _y * _y + _z * _z);
+
+	if (norm == 0) return;
+
+	_x /= norm;
+	_y /= norm;
+	_z /= norm;
+}
+
 // Returns a string representation of this vector.
 const std::string Vec3::str()
 {
 	std::ostringstream rtn;
-	rtn.precision(2);
+	rtn.precision(5);
 	rtn << '[' << _x << ", " << _y << ", " << _z << ']';
 	return rtn.str();
 }
 
 // Add two vectors.
-const Vec3 Vec3::operator+(const Vec3& a)
+const Vec3 Vec3::operator+(const Vec3& a) const
 {
 	return Vec3(a._x + _x, a._y + _y, a._z + _z);
 }
@@ -42,7 +55,7 @@ Vec3 Vec3::operator+=(const Vec3& a)
 }
 
 // Subtract two vectors.
-const Vec3 Vec3::operator-(const Vec3& a)
+const Vec3 Vec3::operator-(const Vec3& a) const
 {
 	return Vec3(_x - a._x, _y - a._y, _z - a._z);
 }
@@ -58,15 +71,27 @@ Vec3 Vec3::operator-=(const Vec3& a)
 }
 
 // Dot product of two vectors.
-const Vec3 Vec3::operator*(const Vec3& a)
+const double Vec3::operator*(const Vec3& a) const
 {
-	return Vec3(_y * a._z - _z * a._y, _z * a._x - _x * a._z, _x * a._y - _y * a._x);
+	return _x * a._x + _y * a._y + _z * a._z;
+}
+
+// Multiply a scalar by each component of the vector.
+const Vec3 Vec3::operator*(const double a) const
+{
+	return Vec3(_x * a, _y * a, _z * a);
+}
+
+// Multiply a scalar by each component of the vector.
+const Vec3 operator*(const double a, const Vec3& b)
+{
+	return b * a;
 }
 
 // Cross product of two vectors.
-const Vec3 Vec3::operator^(const Vec3& a)
+const Vec3 Vec3::operator^(const Vec3& a) const
 {
-	return Vec3(_x - a._x, _y - a._y, _z - a._z);
+	return Vec3(_y * a._z - _z * a._y, _z * a._x - _x * a._z, _x * a._y - _y * a._x);
 }
 
 // Assign one vector to another (copy it).
