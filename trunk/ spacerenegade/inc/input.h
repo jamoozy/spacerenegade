b07eae4c4fb58f7feff0cac7d288977bc880b4a9
jamoozy <jamoozy@d98d4265-742f-0410-abd1-c3b8cf015911>
2007-05-07 08:05:28 +0000

    Changed up the framework to allow for multiple key presses.  Now the key pressing is both more fluid and more complex =).

--- trunk/ spacerenegade/inc/input.h
+++ trunk/ spacerenegade/inc/input.h
@@ -1,7 +1,32 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+enum { SR_KEY_A = 0, SR_KEY_B, SR_KEY_C, SR_KEY_D, SR_KEY_E, SR_KEY_F, SR_KEY_G,
+	   SR_KEY_H, SR_KEY_I, SR_KEY_J, SR_KEY_K, SR_KEY_L, SR_KEY_M, SR_KEY_N,
+	   SR_KEY_O, SR_KEY_P, SR_KEY_Q, SR_KEY_R, SR_KEY_S, SR_KEY_T, SR_KEY_U,
+	   SR_KEY_V, SR_KEY_W, SR_KEY_X, SR_KEY_Y, SR_KEY_Z, SR_KEY_SPACE, SR_NUM_KEYS };
+
+class Keyboard
+{
+	bool *keys;
+	static Keyboard *keyboard;
+	Keyboard();
+
+public:
+
+	static Keyboard *getKeyboard();
+	static void cleanUp();
+
+	~Keyboard();
+
+	bool isDown(int key);
+	void setDown(int key);
+	void setUp(int key);
+};
+
 void readKeyboard(unsigned char key, int x, int y);
+
+void readKeyboardUp(unsigned char key, int x, int y);
 
 void readSpecialKeys(int key, int x, int y);
 
--- trunk/ spacerenegade/src/input.cpp
+++ trunk/ spacerenegade/src/input.cpp
@@ -3,44 +3,72 @@
 #include "input.h"
 #include "ship.h"
 
+Keyboard *Keyboard::keyboard = NULL;
+
+Keyboard::Keyboard() : keys(new bool[SR_NUM_KEYS])
+{
+	for (int i = 0; i < 26; i++)
+		keys[i] = false;
+}
+
+Keyboard::~Keyboard()
+{
+	delete [] keys;
+}
+
+Keyboard *Keyboard::getKeyboard()
+{
+	if (keyboard == NULL)
+		keyboard = new Keyboard();
+
+	return keyboard;
+}
+
+bool Keyboard::isDown(int key)
+{
+	return keys[key];
+}
+
+void Keyboard::setDown(int key)
+{
+	keys[key] = true;
+}
+
+void Keyboard::setUp(int key)
+{
+	keys[key] = false;
+}
+
+void Keyboard::cleanUp()
+{
+	delete keyboard;
+}
+
 extern Ship *playerShip;
 
 void readKeyboard(unsigned char key, int x, int y)
 {
-	switch (key)
-	{
-		case 'q':
-		case 'Q':
-			exit(0);
-			break;
+	if ('a' <= key && key <= 'z')
+		Keyboard::getKeyboard()->setDown(key - 'a');
+	else if ('A' <= key && key <= 'Z')
+		Keyboard::getKeyboard()->setDown(key - 'A');
+	else if (key == ' ')
+		Keyboard::getKeyboard()->setDown(SR_KEY_SPACE);
+	else
+		std::cout << "got a " << key << " for some reason." << std::endl;
+}
 
-		case 'e':
-		case 'E':
-			playerShip->accelerate();
-			break;
 
-		case 'd':
-		case 'D':
-			playerShip->decelerate();
-			break;
-
-		case 's':
-		case 'S':
-			playerShip->yawLeft();
-			break;
-
-		case 'f':
-		case 'F':
-			playerShip->yawRight();
-			break;
-
-		case 'g':
-		case 'G':
-			playerShip->stabilize();
-			break;
-	}
-
-	glutPostRedisplay();
+void readKeyboardUp(unsigned char key, int x, int y)
+{
+	if ('a' <= key && key <= 'z')
+		Keyboard::getKeyboard()->setUp(key - 'a');
+	else if ('A' <= key && key <= 'Z')
+		Keyboard::getKeyboard()->setUp(key - 'A');
+	else if (key == ' ')
+		Keyboard::getKeyboard()->setUp(SR_KEY_SPACE);
+	else
+		std::cout << "got a " << key << " for some reason." << std::endl;
 }
 
 void readSpecialKeys(int key, int x, int y)
@@ -48,12 +76,10 @@ void readSpecialKeys(int key, int x, int y)
 //	glutPostRedisplay();
 }
 
-
 void mouseButtHandler(int button, int state, int x, int y)
 {
 //	glutPostRedisplay();
 }
-
 
 void mouseMoveHandler(int x, int y)
 {
