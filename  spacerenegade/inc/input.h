39a9abf24ff200234b9aadbbe319ec15da040d3e
jamoozy <jamoozy@d98d4265-742f-0410-abd1-c3b8cf015911>
2007-05-09 08:22:24 +0000

    Added the F-keys, number keys, and the rest of the special keys to the keyboard object.  Yay for me.

---  spacerenegade/inc/input.h
+++  spacerenegade/inc/input.h
@@ -4,18 +4,40 @@
 enum { SR_KEY_A = 0, SR_KEY_B, SR_KEY_C, SR_KEY_D, SR_KEY_E, SR_KEY_F, SR_KEY_G,
 	   SR_KEY_H, SR_KEY_I, SR_KEY_J, SR_KEY_K, SR_KEY_L, SR_KEY_M, SR_KEY_N,
 	   SR_KEY_O, SR_KEY_P, SR_KEY_Q, SR_KEY_R, SR_KEY_S, SR_KEY_T, SR_KEY_U,
-	   SR_KEY_V, SR_KEY_W, SR_KEY_X, SR_KEY_Y, SR_KEY_Z, SR_KEY_SPACE, SR_NUM_KEYS };
+	   SR_KEY_V, SR_KEY_W, SR_KEY_X, SR_KEY_Y, SR_KEY_Z,
+
+       SR_KEY_SPACE, 
+
+       SR_KEY_F1, SR_KEY_F2, SR_KEY_F3, SR_KEY_F4, SR_KEY_F5, SR_KEY_F6,
+       SR_KEY_F7, SR_KEY_F8, SR_KEY_F9, SR_KEY_F10, SR_KEY_F11, SR_KEY_F12,
+
+       SR_KEY_1, SR_KEY_2, SR_KEY_3, SR_KEY_4, SR_KEY_5, SR_KEY_6,
+       SR_KEY_7, SR_KEY_8, SR_KEY_9, SR_KEY_10, SR_KEY_11, SR_KEY_12,
+
+       SR_KEY_LEFT, SR_KEY_UP, SR_KEY_RIGHT, SR_KEY_DOWN,
+	   
+       SR_KEY_PAGE_UP, SR_KEY_PAGE_DOWN, SR_KEY_HOME, SR_KEY_END, SR_KEY_INSERT,
+
+       SR_NUM_KEYS };
 
 class Keyboard
 {
-	bool *keys;
 	static Keyboard *keyboard;
+
+public:
+	static Keyboard *getKeyboard();
+	static void cleanUp();
+
+
+
+// ---- Object part ------
+
+private:
+	bool *keys;
 	Keyboard();
 
 public:
 
-	static Keyboard *getKeyboard();
-	static void cleanUp();
 
 	~Keyboard();
 
@@ -25,13 +47,12 @@ public:
 };
 
 void readKeyboard(unsigned char key, int x, int y);
-
 void readKeyboardUp(unsigned char key, int x, int y);
 
 void readSpecialKeys(int key, int x, int y);
+void readSpecialKeysUp(int key, int x, int y);
 
 void mouseButtHandler(int button, int state, int x, int y);
-
 void mouseMoveHandler(int x, int y);
 
 #endif
---  spacerenegade/src/input.cpp
+++  spacerenegade/src/input.cpp
@@ -52,10 +52,12 @@ void readKeyboard(unsigned char key, int x, int y)
 		Keyboard::getKeyboard()->setDown(key - 'a');
 	else if ('A' <= key && key <= 'Z')
 		Keyboard::getKeyboard()->setDown(key - 'A');
+	else if ('0' <= key && key <= '9')
+		Keyboard::getKeyboard()->setDown(key - '0');
 	else if (key == ' ')
 		Keyboard::getKeyboard()->setDown(SR_KEY_SPACE);
 	else
-		std::cout << "got a " << key << " for some reason." << std::endl;
+		std::cout << "got a " << key << " down." << std::endl;
 }
 
 
@@ -65,15 +67,72 @@ void readKeyboardUp(unsigned char key, int x, int y)
 		Keyboard::getKeyboard()->setUp(key - 'a');
 	else if ('A' <= key && key <= 'Z')
 		Keyboard::getKeyboard()->setUp(key - 'A');
+	else if ('0' <= key && key <= '9')
+		Keyboard::getKeyboard()->setUp(key - '0');
 	else if (key == ' ')
 		Keyboard::getKeyboard()->setUp(SR_KEY_SPACE);
 	else
-		std::cout << "got a " << key << " for some reason." << std::endl;
+		std::cout << "got a " << key << " up." << std::endl;
 }
 
 void readSpecialKeys(int key, int x, int y)
 {
-//	glutPostRedisplay();
+	switch (key)
+	{
+		case GLUT_KEY_F1: Keyboard::getKeyboard()->setDown(SR_KEY_F1); break;
+		case GLUT_KEY_F2: Keyboard::getKeyboard()->setDown(SR_KEY_F2); break;
+		case GLUT_KEY_F3: Keyboard::getKeyboard()->setDown(SR_KEY_F3); break;
+		case GLUT_KEY_F4: Keyboard::getKeyboard()->setDown(SR_KEY_F4); break;
+		case GLUT_KEY_F5: Keyboard::getKeyboard()->setDown(SR_KEY_F5); break;
+		case GLUT_KEY_F6: Keyboard::getKeyboard()->setDown(SR_KEY_F6); break;
+		case GLUT_KEY_F7: Keyboard::getKeyboard()->setDown(SR_KEY_F7); break;
+		case GLUT_KEY_F8: Keyboard::getKeyboard()->setDown(SR_KEY_F8); break;
+		case GLUT_KEY_F9: Keyboard::getKeyboard()->setDown(SR_KEY_F9); break;
+		case GLUT_KEY_F10: Keyboard::getKeyboard()->setDown(SR_KEY_F10); break;
+		case GLUT_KEY_F11: Keyboard::getKeyboard()->setDown(SR_KEY_F11); break;
+		case GLUT_KEY_F12: Keyboard::getKeyboard()->setDown(SR_KEY_F12); break;
+
+		case GLUT_KEY_LEFT: Keyboard::getKeyboard()->setDown(SR_KEY_LEFT); break;
+		case GLUT_KEY_UP: Keyboard::getKeyboard()->setDown(SR_KEY_UP); break;
+		case GLUT_KEY_RIGHT: Keyboard::getKeyboard()->setDown(SR_KEY_RIGHT); break;
+		case GLUT_KEY_DOWN: Keyboard::getKeyboard()->setDown(SR_KEY_DOWN); break;
+
+		case SR_KEY_PAGE_UP: Keyboard::getKeyboard()->setDown(SR_KEY_PAGE_UP); break;
+		case SR_KEY_PAGE_DOWN: Keyboard::getKeyboard()->setDown(SR_KEY_PAGE_DOWN); break;
+		case SR_KEY_HOME: Keyboard::getKeyboard()->setDown(SR_KEY_HOME); break;
+		case SR_KEY_END: Keyboard::getKeyboard()->setDown(SR_KEY_END); break;
+		case SR_KEY_INSERT: Keyboard::getKeyboard()->setDown(SR_KEY_INSERT); break;
+	}
+}
+
+void readSpecialKeysUp(int key, int x, int y)
+{
+	switch (key)
+	{
+		case GLUT_KEY_F1: Keyboard::getKeyboard()->setUp(SR_KEY_F1); break;
+		case GLUT_KEY_F2: Keyboard::getKeyboard()->setUp(SR_KEY_F2); break;
+		case GLUT_KEY_F3: Keyboard::getKeyboard()->setUp(SR_KEY_F3); break;
+		case GLUT_KEY_F4: Keyboard::getKeyboard()->setUp(SR_KEY_F4); break;
+		case GLUT_KEY_F5: Keyboard::getKeyboard()->setUp(SR_KEY_F5); break;
+		case GLUT_KEY_F6: Keyboard::getKeyboard()->setUp(SR_KEY_F6); break;
+		case GLUT_KEY_F7: Keyboard::getKeyboard()->setUp(SR_KEY_F7); break;
+		case GLUT_KEY_F8: Keyboard::getKeyboard()->setUp(SR_KEY_F8); break;
+		case GLUT_KEY_F9: Keyboard::getKeyboard()->setUp(SR_KEY_F9); break;
+		case GLUT_KEY_F10: Keyboard::getKeyboard()->setUp(SR_KEY_F10); break;
+		case GLUT_KEY_F11: Keyboard::getKeyboard()->setUp(SR_KEY_F11); break;
+		case GLUT_KEY_F12: Keyboard::getKeyboard()->setUp(SR_KEY_F12); break;
+
+		case GLUT_KEY_LEFT: Keyboard::getKeyboard()->setUp(SR_KEY_LEFT); break;
+		case GLUT_KEY_UP: Keyboard::getKeyboard()->setUp(SR_KEY_UP); break;
+		case GLUT_KEY_RIGHT: Keyboard::getKeyboard()->setUp(SR_KEY_RIGHT); break;
+		case GLUT_KEY_DOWN: Keyboard::getKeyboard()->setUp(SR_KEY_DOWN); break;
+
+		case SR_KEY_PAGE_UP: Keyboard::getKeyboard()->setUp(SR_KEY_PAGE_UP); break;
+		case SR_KEY_PAGE_DOWN: Keyboard::getKeyboard()->setUp(SR_KEY_PAGE_DOWN); break;
+		case SR_KEY_HOME: Keyboard::getKeyboard()->setUp(SR_KEY_HOME); break;
+		case SR_KEY_END: Keyboard::getKeyboard()->setUp(SR_KEY_END); break;
+		case SR_KEY_INSERT: Keyboard::getKeyboard()->setUp(SR_KEY_INSERT); break;
+	}
 }
 
 void mouseButtHandler(int button, int state, int x, int y)
