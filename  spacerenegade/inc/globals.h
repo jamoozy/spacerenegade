c068fdfde75628ea01adb7cde0a8b2d16c78b34e
jamoozy <jamoozy@d98d4265-742f-0410-abd1-c3b8cf015911>
2007-05-13 19:29:50 +0000

    Changed the structure of the game to include the octtree.  The octtree draws everything in it (that I've put in it so far, anyway), but I have not yet made sure that everything is in the correct nodes.

---  spacerenegade/inc/globals.h
+++  spacerenegade/inc/globals.h
@@ -1,10 +1,11 @@
-#ifndef GLOBALS_H
-#define GLOBALS_H
+//#ifndef GLOBALS_H
+//#define GLOBALS_H
+//
+//#include "asteroid.h"
+//#include "ship.h"
+//
+//Asteroid *asteroids;
+//Ship *playerShip;
+//
+//#endif
 
-#include "asteroid.h"
-#include "ship.h"
-
-Asteroid *asteroids;
-Ship *playerShip;
-
-#endif
