82723ab566f3e41379ba8fe37053cd5a407e4767
jamoozy <jamoozy@d98d4265-742f-0410-abd1-c3b8cf015911>
2007-05-19 10:20:29 +0000

    Changed the interface a bit to allow for better button pushing stuff :)

--- trunk/ spacerenegade/inc/button.h
+++ trunk/ spacerenegade/inc/button.h
@@ -14,26 +14,27 @@ private:
 	GLfloat xPos;
 	GLfloat yPos;
 	GLfloat RGB[3];
-	int id;         // The ID number (for picking purposes).
+	GLuint id;         // The ID number (for picking purposes).
+	void (*pressFunc)(); // Function to call when button is pressed.
 
 public:
 	Button();
 	Button(string title, GLfloat buffer,
 	       GLfloat xPos, GLfloat yPos,
 	       GLfloat red, GLfloat green, GLfloat blue,
-	       int id, void (*buttonPressed)());
+	       GLuint id, void (*pressFunc)());
 	virtual ~Button();
 
-	void Place();
+	void Place(GLenum mode);
 	void DrawText(GLint x, GLint y, string s, GLfloat r, GLfloat g, GLfloat b);
 
 	GLfloat getBuffer(){return buffer;}
 	string getTitle(){return title;}
 	GLfloat getXPos(){return xPos;}
 	GLfloat getYPos(){return yPos;}
-	int getID() { return id; }
+	GLuint getID() { return id; }
 
-	void (*buttonPressed)(); // Function to call when button is pressed.
+	void buttonPressed();
 	void setBuffer(GLfloat buffer){buffer = buffer;}
 	void setTitle(string title){title = title;}
 	void setXPos(GLfloat xPos){xPos = xPos;}
