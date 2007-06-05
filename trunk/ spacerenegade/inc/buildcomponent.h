1ab1b853a0934b6325621ac3dec52cff623c8f17
jamoozy <jamoozy@d98d4265-742f-0410-abd1-c3b8cf015911>
2007-06-05 08:09:56 +0000

    - Put in Material object.  It is the base class for the types of raw building materials (e.g. Einsteinium).  Doesn't quite work as I'd like it to, though.

--- trunk/ spacerenegade/inc/buildcomponent.h
+++ trunk/ spacerenegade/inc/buildcomponent.h
@@ -2,22 +2,17 @@
 #ifndef BUILDCOMPONENT_H
 #define BUILDCOMPONENT_H
 
-//#include "object.h"
-//#include "vec3.h"
+#include "object.h"
 
-class BuildComponent
+class Material : public Object
 {
-private:
-
-	//Vec3 angle;
-	//Vec3 avelocity;
-
 public:
+	Material(const Vec3& pos, const Vec3& vel) : Object("", pos, vel) {};
+	virtual ~Material() {};
+	virtual string getType() { return "Material"; };
 
-	//Asteroid();
-	//Asteroid(double px, double py, double pz, double vx, double vy, double vz);
-	//Asteroid(const Vec3& pos, const Vec3& v);
-	//void draw();
+	virtual void draw();
+	virtual void hits() {};
 };
 
 #endif
--- trunk/ spacerenegade/src/buildcomponent.cpp
+++ trunk/ spacerenegade/src/buildcomponent.cpp
@@ -1,4 +1,17 @@
+#include "GL/glut.h"
+#include "buildcomponent.h"
 
-//BuildComponent::BuildComponent()
-//{}
+void Material::draw()
+{
+	position += velocity;
+
+	glDisable(GL_TEXTURE_2D);
+	glPushMatrix();
+
+	glColor3f(1,1,1);
+	glTranslated(position.x(), position.y(), position.z());
+	glutSolidSphere(1,10,4);
+
+	glPopMatrix();
+}
 
