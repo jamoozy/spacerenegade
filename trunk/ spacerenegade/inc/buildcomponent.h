33a3a6e59a40ab34bdc6fd9018e22f4db29615ee
jamoozy <jamoozy@d98d4265-742f-0410-abd1-c3b8cf015911>
2007-06-12 05:43:23 +0000

    Adjusted the radius of material (for collision detection).

--- trunk/ spacerenegade/inc/buildcomponent.h
+++ trunk/ spacerenegade/inc/buildcomponent.h
@@ -7,7 +7,7 @@
 class Material : public Object
 {
 public:
-	Material(const Vec3& pos, const Vec3& vel) : Object("", pos, vel, 0,0,1) {};
+	Material(const Vec3& pos, const Vec3& vel) : Object("", pos, vel, 0,0,1) { radius = 1.5; };
 	virtual ~Material() {};
 	virtual string getType() { return "Material"; };
 
