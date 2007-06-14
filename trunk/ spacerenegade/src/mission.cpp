1a14c9ee966649e78c9e4686e6a240e2b3221e67
GumOtaku <GumOtaku@d98d4265-742f-0410-abd1-c3b8cf015911>
2007-06-14 11:39:33 +0000

    Just added harder better faster stronger mission descriptions.

--- trunk/ spacerenegade/src/mission.cpp
+++ trunk/ spacerenegade/src/mission.cpp
@@ -26,22 +26,22 @@ Mission::Mission(int id) : id(id), title("Title goes here"), briefing("Briefing 
 	switch (id)
 	{
 		case 0:
-			title = "Help Crooky get his revenge!";
-			briefing = "My name is Crooky and I want you to kill reds. Reds killed my pa. You kill reds now plz kthxbai. ";
-			completeBriefing = "Thanks for killing those reds for me.  \n \n Here is 5000 credits.";
+			title = "A Test of Honor";
+			briefing = "Greetings, traveler. I understand you are looking for work.\n\nMy name is Lieutenant Crooky, and we are looking for more Blue soldiers. If you want to join our alliance, prove your devotion by destroying two Red ships. We'll reward not only with 10,000 credits, but an opportunity to one day lead the entire Blue faction into glory. ";
+			completeBriefing = "Well done. You are clearly devoted to our cause. Expect to see more offers from us in the future, with higher stakes and higher rewards.  \n \n Here is 5000 credits, as promised.";
 			objectives.push_back(new BountyObjective("Red Ships", 0, 2));
 
-			reward = 5000;
+			reward = 10000;
 			completed = false;
 			break;
 
 		case 1:
-			title = "Help Bumbum get his revenge!";
-			briefing = "My name is Bumbum and I want you to kill blues. Blues killed my ma. You kill blues now plz kthxbai. ";
-			completeBriefing = "Thanks for killing those blues for me.  \n \n Here is 5000 credits.";
+			title = "Blood Red";
+			briefing = "Hey, kid! Your face tells me you're looking for money. Well, we've got the money if you've got the guts.\n\nThey call me Bumbum the Corpulent, for I am the leader of the mightiest faction in the galaxy: The Reds. If you wanna join the party, you gotta have guts. Shoot down two Blue ships and you'll get 10,000 credits... and we may just call you a fellow Red. ";
+			completeBriefing = "Ha! You sure know how to party. If you're hungry for more, drop by the mission board some other time.  \n \n Here's 10,000 clams. Don't spend it all in one place! HAHAHA.";
 			objectives.push_back(new BountyObjective("Blue Ships", 1, 2));
 
-			reward = 5000;
+			reward = 10000;
 			completed = false;
 			break;
 	}
@@ -76,7 +76,7 @@ bool Mission::isCompleted() const
 	return complete;
 }
 
-// Automatically inserts "\" at every width'th character,
+// Automatically inserts "\n" at every width'th character,
 // formatting to break at spaces.
 string Mission::insertNewLines(string b, int width)
 {
@@ -94,10 +94,20 @@ string Mission::insertNewLines(string b, int width)
 		else
 		{
 			chunk = b.substr(k,width+1);
-			spaceIndex = chunk.find_last_of(" ");
-			chunk = chunk.substr(0, spaceIndex);
-			newB.append(chunk + "\n");
-			k += spaceIndex + 1;
+			spaceIndex = chunk.find_last_of("\n");
+			if (spaceIndex > -1) // if there is a "\n" in this chunk (possible bug: if chunk is "blahblah\" and rest of string is "nblahblah". Oh well, easier to avoid than fix at the moment.)
+			{
+				chunk = chunk.substr(0, spaceIndex + 2);
+				newB.append(chunk);
+				k += spaceIndex + 2;
+			}
+			else
+			{
+				spaceIndex = chunk.find_last_of(" ");
+				chunk = chunk.substr(0, spaceIndex);
+				newB.append(chunk + "\n");
+				k += spaceIndex + 1;
+			}
 		}
 	}
 	return newB;
